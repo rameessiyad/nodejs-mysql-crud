7a05efedfc083a43dd4d7566d1e644dfa20123da
rameessiyad <rameesmsiyadofficial786@gmail.com>
2024-11-06 10:45:56 +0530

    category controller finished

--- server/controllers/category-controller.js
+++ server/controllers/category-controller.js
@@ -25,4 +25,49 @@ module.exports = {
       data: category,
     });
   },
+
+  //get all categories
+  getCategories: async (req, res) => {
+    const categories = await prisma.category.findMany();
+    return res.status(200).json({ success: true, data: categories });
+  },
+
+  //get by id
+  getCategory: async (req, res) => {
+    const { id } = req.params;
+    const category = await prisma.category.findUnique({
+      where: { id: parseInt(id) },
+    });
+
+    return res.status(200).json({ success: true, data: category });
+  },
+
+  //update category
+  updateCategory: async (req, res) => {
+    const { id } = req.params;
+    const { name } = req.body;
+
+    const existingCategory = await prisma.category.findUnique({
+      where: { id: parseInt(id) }
+    })
+
+    if (!existingCategory) return res.status(400).json({ message: "Category not found" });
+
+    const updatedCategory = await prisma.category.update({
+      where: { id: parseInt(id) },
+      data: { name },
+    })
+
+    return res.status(200).json({ message: "Category updated successfully", data: updatedCategory })
+  },
+
+  //delete category
+  deleteCategory: async (req, res) => {
+    const { id } = req.params;
+    const category = await prisma.category.delete({
+      where: { id: parseInt(id) },
+    });
+
+    return res.status(200).json({ success: true, message: "Category deleted" });
+  },
 };
--- server/routes/category-route.js
+++ server/routes/category-route.js
@@ -1,8 +1,18 @@
 const express = require("express");
-const { createCategory } = require("../controllers/category-controller");
+const {
+  createCategory,
+  getCategories,
+  getCategory,
+  updateCategory,
+  deleteCategory,
+} = require("../controllers/category-controller");
 
 const router = express.Router();
 
 router.post("/", createCategory);
+router.get("/", getCategories);
+router.get("/:id", getCategory);
+router.patch("/:id", updateCategory);
+router.delete("/:id", deleteCategory);
 
 module.exports = router;
